1125baeacbc45dd391bae71aaae927b35fc66caa
Finn Bacall <finn.bacall@cs.man.ac.uk>
2012-09-26 10:48:04 +0100

    Added a bit of documentation. Tweaked check method

--- lib/transformation-client.rb
+++ lib/transformation-client.rb
@@ -4,7 +4,8 @@ require 'json'
 module Wf4Ever
   class TransformationClient
 
-    # Returns URI of job for future checking
+    ##
+    # Creates a transformation job and returns its URI.
     def self.create_job(uri, resource, format, ro, token)
       uri = URI(uri)
       job_uri = nil
@@ -29,7 +30,12 @@ module Wf4Ever
       job_uri
     end
 
-    # Returns status of job
+    ##
+    # Returns a hash containing: 
+    # ["resource"]  the resource being transformed
+    # ["format"]    the content type of the resource
+    # ["ro"]        the URI of the RO being created
+    # ["status"]    the status of the transformation job  
     def self.check_job(uri)
       uri = URI(uri)
       Net::HTTP.start(uri.host, uri.port || 80) do |http|
@@ -38,14 +44,15 @@ module Wf4Ever
 
         if response.code == "200"
           response_body = JSON.parse(response.body)
-          response_body["status"]
+          response_body
         else
           raise "#{response.code} #{response.body}"
         end
       end
     end
 
-    # Cancels job and returns true
+    ##
+    # Cancels job and returns true if it succeeds.
     def self.cancel_job(uri)
       uri = URI(uri)
       Net::HTTP.start(uri.host, uri.port || 80) do |http|
