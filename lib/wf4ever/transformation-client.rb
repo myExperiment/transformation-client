0117f8163862e22724e6f9cf803a537592bb676f
Stian Soiland-Reyes <stian@soiland-reyes.com>
2013-12-20 16:02:06 +0000

    Support extract folders

--- lib/wf4ever/transformation-client.rb
+++ lib/wf4ever/transformation-client.rb
@@ -5,18 +5,51 @@ module Wf4Ever
   class TransformationClient
 
     ##
-    # Creates a transformation job and returns its URI.
-    def self.create_job(uri, resource, format, ro, token)
+    # Create a transformation job and returns its URI, which can be
+    # used with check_job() and cancel_job()
+    #
+    # uri -- The URI for the wf-ro service endpoint, e.g.
+    #   "http://example.com/wf-ro/jobs"
+    # resource - The URI for the workflow resource to transform, e.g.
+    #   "http://www.example.net/workflow.t2flow"
+    # format - the media type of the resource, e.g.
+    #   "application/vnd.taverna.t2flow+xml"
+    # ro - the URI for the research object within an RODL service,
+    #   where the transformed and extracted resources are to be uploaded
+    # token - token for authenticating access to the RODL
+    # extract -
+    #   (optional) hash of folders where to extract additional
+    #   resources. The main workflow is always extracted, but will
+    #   only be placed into a folder if it is given in the hash. Other
+    #   resource types are only extracted if listed in the map. 
+    #
+    #   The extract keys should be one of:
+    #     - :main -- transformed workflow 
+    #     - :nested -- nested workflows
+    #     - :scripts -- embedded scripts (e.g. beanshell, R)
+    #     - :services -- external services (e.g. WSDL, REST)
+    #   
+    #   The extract values are URIs to RODL folders within the RO where to
+    #   extract the resource. A value using the keyword :ro means to
+    #   extract the particular resource without aggregating it in any 
+    #   particular folder.
+    def self.create_job(uri, resource, format, ro, token, extract={:main=>:ro})
       uri = URI(uri)
       job_uri = nil
+      ## Replace :ro with the ro URI as per wf-ro REST API
+      # We'll do this in a new hash to not modify our incoming hash
+      # TODO: Avoid Perl-like syntax below..
+      extract = extract.each_with_object({}) { |(k,v), h|  h[k] = v==:ro ? ro : v } 
+
       Net::HTTP.start(uri.host, uri.port || 80) do |http|
         body = {
           "resource" => resource,
           "format" => format,
           "ro" => ro,
-          "token" => token
+          "token" => token,
+          "extract" => extract
         }.to_json
-
+        
         headers = {"content-type" => "application/json"}
 
         response = http.post(uri.request_uri, body, headers)
@@ -31,11 +64,14 @@ module Wf4Ever
     end
 
     ##
-    # Returns a hash containing: 
+    # Check wf-ro transformation job status.
+    #
+    # Return a hash containing: 
     # ["resource"]  the resource being transformed
     # ["format"]    the content type of the resource
     # ["ro"]        the URI of the RO being created
     # ["status"]    the status of the transformation job  
+    # ["extract"]   the resource type to extract, to which folder
     def self.check_job(uri)
       uri = URI(uri)
       Net::HTTP.start(uri.host, uri.port || 80) do |http|
@@ -52,7 +88,7 @@ module Wf4Ever
     end
 
     ##
-    # Cancels job and returns true if it succeeds.
+    # Cancel wf-ro transformation job and returns true if it succeeds.
     def self.cancel_job(uri)
       uri = URI(uri)
       Net::HTTP.start(uri.host, uri.port || 80) do |http|
--- test/test_transformation-client.rb
+++ test/test_transformation-client.rb
@@ -9,8 +9,42 @@ class TestTransformationClient < Test::Unit::TestCase
   RO = "http://sandbox.wf4ever-project.org/rodl/ROs/FinnsTest/"
 
   def test_can_create
-    uri = Wf4Ever::TransformationClient.create_job(SERVICE_URI, RESOURCE, T2FLOW_FORMAT, RO, OAUTH_TOKEN)
-    assert_not_nil uri
+    @uri = Wf4Ever::TransformationClient.create_job(SERVICE_URI, RESOURCE, T2FLOW_FORMAT, RO, OAUTH_TOKEN)
+    assert_not_nil @uri
+    status = Wf4Ever::TransformationClient.check_job(@uri)
+    #puts status
+    assert_equal(RESOURCE, status["resource"])
+    assert_equal(T2FLOW_FORMAT, status["format"])
+    assert_equal(RO, status["ro"])
+    # :ro default should become 'main'
+    assert_equal(RO, status["extract"]["main"])
+    ## The remaining folders should not be listed
+    assert_nil(status["extract"]["nested"])
+    assert_nil(status["extract"]["scripts"])
+    assert_nil(status["extract"]["services"])
+  end
+
+  def setup
+      @uri = nil
+  end
+
+  def teardown
+    Wf4Ever::TransformationClient.cancel_job(@uri) if @uri
+  end
+
+  def test_with_extract
+    extract = {:nested => RO + "folderNested", :scripts => :ro, :main => RO + "folderMain"} 
+    @uri = Wf4Ever::TransformationClient.create_job(SERVICE_URI, RESOURCE, T2FLOW_FORMAT, RO, OAUTH_TOKEN, extract)
+    assert_not_nil @uri
+    status = Wf4Ever::TransformationClient.check_job(@uri)
+#    puts status
+    assert_equal(RESOURCE, status["resource"])
+    assert_equal(T2FLOW_FORMAT, status["format"])
+    assert_equal(RO, status["ro"])
+    assert_equal(RO + "folderMain", status["extract"]["main"])
+    assert_equal(RO + "folderNested", status["extract"]["nested"])
+    assert_equal(RO, status["extract"]["scripts"])
+    assert_nil(status["extract"]["services"])
   end
 
 end
